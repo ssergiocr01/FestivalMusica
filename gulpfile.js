11e646be25591e353b1d0bf28c742a470b34351b
Sergio Serrano Vargas <ssergio@redmivah.local>
2022-11-08 10:41:25 -0600

    add-gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -86,4 +86,4 @@ exports.js = javascript;
 exports.imagenes = imagenes;
 exports.versionWebp = versionWebp;
 exports.versionAvif = versionAvif;
-exports.dev = parallel( imagenes, versionWebp, versionAvif, javascript, dev) ;
+exports.dev = parallel( imagenes, versionWebp, versionAvif, javascript, dev);
